72cd3db325029fc7a93515bba0412ccfe3bb5376
josefinaPorolli <j.porolli@alumno.um.edu.ar>
2025-08-27 08:57:48 -0300

    función resta

--- test.py
+++ test.py
@@ -1,10 +1,14 @@
 import unittest
 from main import suma
+from main import resta
 
 class TestPrueba(unittest.TestCase):
 
     def test_suma(self):
         self.assertEqual(suma(2,3), 5)
+
+    def test_resta(self):
+        self.assertEqual(resta(5,2), 3)
 
 if __name__== "__main__":
     unittest.main()
